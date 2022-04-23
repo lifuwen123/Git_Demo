ae85adc85718b2b0bd9fed8114364bb201e024a2
lifuwen <961222512@qq.com>
2022-04-23 10:12:22 +0800

    first commit

---
+++ src/main/java/com/laoli/GitTest.java
@@ -0,0 +1,14 @@
+package com.laoli;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: lfw
+ * @Date: 2022/04/23/10:01
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("我的Git第一次测试");
+    }
+}
---
+++ src/main/java/com/laoli/TTT.java
@@ -0,0 +1,11 @@
+package com.laoli;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: lfw
+ * @Date: 2022/04/23/10:11
+ * @Description:
+ */
+public class TTT {
+}
