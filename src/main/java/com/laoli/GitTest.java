ce58e896e98bf90d60f7e68955f7d8e4d3922a11
lifuwen <961222512@qq.com>
2022-04-23 10:25:44 +0800

    hot-fix commit

--- src/main/java/com/laoli/GitTest.java
+++ src/main/java/com/laoli/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我的Git第一次测试");
         System.out.println("我的Git第二次测试");
+        System.out.println("我的Git第三次测试");
     }
 }
